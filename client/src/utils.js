f116c30a3e16745bc1b290e55e2516ba31571a42
Michael <none@none.org>
2017-04-19 16:08:32 -0500

    Add types to utils and fix futures utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,10 +1,39 @@
-import { compose, Future, map } from './prelude';
+// @flow
 
-export const sideEffect = f => d => { f(d); return d; };
-export const P2F = p => Future((rej, res) => p.then(res, rej));
-export const F2P = f => new Promise((res, rej) => f.fork(rej, res));
-export const futurize = fp => (...args) => P2F(fp(...args))
+import { Future, once } from './prelude';
 
-// Poor man's Future.all
-export const Futureall = fs =>
-  compose(P2F, Promise.all, map(F2P));
+export const sideEffect =
+  <A>(f: A => mixed) : (A => A) =>
+     (d: A) =>
+     { f(d); return d; };
+export const P2F =
+  (p: Promise<*>) : Future =>
+  Future((rej, res) => p.then(res, rej)
+    .catch(e => console.error("Uncaught here: ", e)));
+export const F2P =
+  (f: Future) : Promise<*> =>
+  new Promise((res, rej) => f.fork(rej, res));
+export const futurize =
+  (fp: (mixed => Promise<*>)) =>
+  (...args: Array<*>) => Future((rej, res) => fp(...args).then(res, rej));
+
+export const Futureall = (fs: Array<Future>) : Future => {
+  let futuresRemaining = fs.length;
+  if(futuresRemaining === 0)
+    return Future.of([]);
+
+  let results = fs.map(() => undefined);
+
+  return Future((rej, res) => {
+    const childReject = once(rej);
+    const childResolve = i => result => {
+      results[i] = result;
+      futuresRemaining--;
+
+      if(futuresRemaining === 0)
+        res(results);
+    };
+
+    fs.forEach((f, i) => f.fork(childReject, childResolve(i)))
+  });
+};
