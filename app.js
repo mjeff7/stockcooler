f18290a51d21d23964ab62faa1eb630e3f9af225
Michael <none@none.org>
2017-04-24 23:30:44 -0500

    Add echoHistory

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var echoHistory = require('./echoHistory');
 
 var app = express();
 
@@ -15,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/history', echoHistory);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -24,13 +27,15 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.error("Error:");
+  console.error({err});
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.end('error');
 });
 
 module.exports = app;
---
+++ echoHistory.js
@@ -0,0 +1,56 @@
+// @flow
+
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+import { fileCache } from './cache';
+
+
+const data_url : string => string = symbol =>
+  'http://www.google.com/finance/historical' +
+  `?q=${symbol}&startdate=1970-01-01&output=csv`;
+
+const fetchText = symbol =>
+  fetch(data_url(symbol))
+  .then(response => response.ok
+                    ? response
+                    : Promise.reject({ error: 'fetch error',
+                                       statusText: response.statusText,
+                                       response
+                                     }))
+  .then(r => r.text());
+
+
+const mkdirP = pathIn => {
+  const {root, dir, base} = path.parse(pathIn);
+  if(root !== dir)
+    mkdirP(dir);
+
+  try { fs.mkdirSync(pathIn); }
+  catch(e) { if(e.code !== 'EEXIST') throw e; };
+};
+
+const MOCK_DIR = 'mock/cache';
+mkdirP(MOCK_DIR);
+let historyCache = fileCache(MOCK_DIR);
+
+
+const sideEffect = f => x => {
+  f(x);
+  return x;
+};
+
+const getHistory = symbol =>
+  historyCache.has(symbol)
+  ? Promise.resolve(historyCache.get(symbol))
+  : fetchText(symbol)
+    .then(sideEffect(d => historyCache.set(symbol, d)))
+    .catch(error => {
+      console.error({error});
+      return JSON.stringify(error);
+    });
+
+
+module.exports = (req: {query: {q: string}},
+                  res: {end: * => void}) =>
+  getHistory(req.query.q).then(x => res.end(x));
